68d12955804e7aab52abeb6efc20fba2aa1e32a5
Aleksey Goncharov <to-english@yandex.ru>
2025-05-04 22:25:30 +0300

    Lecture 4, task F

--- YandexAlgoTraining_1_0/HomeWork/Lecture_4/Sales/main.js
+++ YandexAlgoTraining_1_0/HomeWork/Lecture_4/Sales/main.js
@@ -1,7 +1,46 @@
 const fs = require('fs');
 const fileContent = fs.readFileSync("input.txt", "utf8");
-const [n, c, k, p] = fileContent.toString().trim().split(/\r?\n/);
+const entries = fileContent.toString().trim().split(/\r?\n/);
 
 
-const result = calcKeyboardResource1(n, c, k, p);
+const result = calcSalesByItem(entries);
 fs.writeFileSync("output.txt", result.toString());
+
+//0.926s, 90.32Mb
+function calcSalesByItem(entries) {
+    const names = {};
+    let sortedNames;
+
+    for (let i = 0; i < entries.length; i++) {
+        let [name, item, vol] = entries[i].split(' ');
+
+        if (names.hasOwnProperty(name)) {
+            if (!names[name].hasOwnProperty(item)) {
+                names[name][item] = 0;
+            }
+        } else {
+            names[name] = {};
+            if (!names[name].hasOwnProperty(item)) {
+                names[name][item] = 0;
+            }
+        }
+        names[name][item] += Number(vol)
+    }
+
+    sortedNames = Object.keys(names).sort().reduce((obj, key) => {
+        obj[key] = names[key]
+        return obj;
+    }, {});
+
+    let resArr = [];
+    for (const name in sortedNames) {
+        let nameItems = [];
+        for (const item in sortedNames[name]) {
+            nameItems.push(`${item} ${sortedNames[name][item]}`);
+        }
+        let sortedItems = nameItems.sort().join('\n');
+        resArr.push(`${name}:\n${sortedItems}`);
+    }
+    return resArr.join('\n');
+
+}
